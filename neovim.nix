fe7da8e9f3cc422ed6d4cac5d9252eb6a33540b7
Matthew Benkart <Mthwate@users.noreply.github.com>
2023-08-13 22:39:45 -0700

    Create neovim.nix
    Add an initial neovim config

---
+++ neovim.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraConfig = ''
+      set shiftwidth=4 smarttab
+      set expandtab
+      set tabstop=4 softtabstop=0
+      set number relativenumber
+    '';
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = nvim-treesitter;
+        type = "lua";
+        config = ''
+          require'nvim-treesitter.configs'.setup {
+            highlight = {
+              enable = true,
+            },
+            additional_vim_regex_highlighting = false,
+          }
+        '';
+      }
+      nvim-treesitter-parsers.c
+      nvim-treesitter-parsers.go
+      nvim-treesitter-parsers.gitcommit
+      nvim-treesitter-parsers.gitignore
+      nvim-treesitter-parsers.git_rebase
+      nvim-treesitter-parsers.lua
+      nvim-treesitter-parsers.nix
+      nvim-treesitter-parsers.python
+      nvim-treesitter-parsers.query
+      nvim-treesitter-parsers.rust
+      nvim-treesitter-parsers.vim
+      nvim-treesitter-parsers.vimdoc
+      nvim-treesitter-context
+    ];
+  };
+}

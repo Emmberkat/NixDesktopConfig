5b2d0acd1569d9865f471dd4f32ce0dffdff9a10
Matthew Benkart <matthewbenkart@gmail.com>
2023-08-20 23:05:02 -0700

    Add LSP completion
    Configure nvim-cmp to enable LSP based completion.

--- neovim.nix
+++ neovim.nix
@@ -35,8 +35,6 @@
         '';
       }
       nvim-lspconfig
-      nvim-cmp
-      cmp-nvim-lsp
       {
         plugin = lsp-zero-nvim;
         type = "lua";
@@ -65,6 +63,36 @@
       nvim-treesitter-parsers.vim
       nvim-treesitter-parsers.vimdoc
       nvim-treesitter-context
+      cmp-nvim-lsp
+      vim-vsnip
+      cmp-vsnip
+      {
+        plugin = nvim-cmp;
+        type = "lua";
+        config = ''
+          local cmp = require'cmp'
+          cmp.setup({
+            snippet = {
+              expand = function(args)
+                vim.fn["vsnip#anonymous"](args.body)
+              end,
+            },
+            mapping = cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.abort(),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = cmp.config.sources({
+              { name = 'nvim_lsp' },
+              { name = 'vsnip' },
+            }, {
+              { name = 'buffer' },
+            }),
+          })
+        '';
+      }
     ];
   };
 }

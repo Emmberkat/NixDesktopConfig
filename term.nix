7d4212f0526bd5c84763e6f666ec7c8269d0196f
Matthew Benkart <Mthwate@users.noreply.github.com>
2024-05-22 22:38:03 -0700

    Create term.nix
    Setup basic wezterm config

---
+++ term.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.mthwate.term;
+in
+{
+  options.mthwate.term.enable = mkEnableOption "term";
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      wezterm
+    ];
+
+    xdg.configFile."wezterm/wezterm.lua".text = ''
+      local wezterm = require 'wezterm'
+      local config = wezterm.config_builder()
+      config.hide_tab_bar_if_only_one_tab = true
+      return config
+    '';
+  };
+}
+
